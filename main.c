82bc97844727a95386decd629193c50586abe5be
Mahmoud Elgazzar <mahmoud.elgazzar@avelabs.com>
2019-03-12 16:33:12 +0200

    Added Main and deleted rubbish files like .o

--- main.c
+++ main.c
@@ -3,77 +3,57 @@
 /**
  * main.c
  */
-
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
 #include "driverlib/debug.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
-#include "driverlib/systick.h"
-#include "driverlib/interrupt.h"
+
+#include "Task.h"
+#include "Schedular.h"
+
+
 uint32_t count = 0;
 
-void SysTickIntHandler();
+#define FIVE_HUNDRED_OS_TICKS 500
+#define ONE_OS_TICK 1
 
 int main(void)
 {
-    /*add Code Here*/
-    volatile uint32_t ui32Loop;
-
-    IntMasterEnable();
-    SysTickIntEnable();
-
-    SysTickIntRegister(SysTickIntHandler);
-
-    SysTickPeriodSet(16000000UL);
-
-    SysTickEnable();
-
-
-
-    //
-    // Enable the GPIO port that is used for the on-board LED.
-    //
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
-    //
-    // Check if the peripheral access is enabled.
-    //
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
-    {
-    }
+      //
+      // Check if the peripheral access is enabled.
+      //
+      while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+      {}
 
-    //
-    // Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
-    //
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+      //
+      // Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
+      // enable the GPIO pin for digital function.
+      //
+      GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
+      GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_2);
+      GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
 
-    // Loop forever.
-    //
+      /*Start Schedulart*/
+      scheduler_Init();
+
+      /*Add Four Tasks*/
+      scheduler_Add_Task(Task1,1000,priority_1,ID_0);
+      scheduler_Add_Task(Task2,2000,priority_1,ID_1);
+      scheduler_Add_Task(Task3,3000,priority_1,ID_2);
+      scheduler_Add_Task(Task4,4000,priority_1,ID_3);
+
+      /*Task To Remove Tasks*/
+      scheduler_Add_Task(Task5,5000,priority_1,ID_4);
+
+      /* Start Schedular */
+      scheduler_Start(ONE_OS_TICK);
     while(1)
     {
-        //
-        // Turn on the LED.
-        //
-        if(count < 1)
-        {
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
-        }
-        if(count > 2)
-        {
-            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x0);
-        }
-        if(count > 3)
-        {
-            count = 0;
-        }
-        }
-}
-void SysTickIntHandler()
-{
-    count++;
-}
+
+    }
+    }
+
